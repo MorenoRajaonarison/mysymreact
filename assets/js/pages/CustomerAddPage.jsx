9658550779dddcfaeda5e296823a999dceeedbf2
Rajaonarison <morenorajaonarison@gmail.com>
2021-04-14 16:15:07 +0200

    MEP systeme de cache pour les cutomers

--- assets/js/pages/CustomerAddPage.jsx
+++ assets/js/pages/CustomerAddPage.jsx
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import Field from "../components/forms/field"
-import { FaSave, FaBackward } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import {FaSave, FaBackward} from "react-icons/fa"
+import {Link} from "react-router-dom"
 import customerApi from "../services/customerApi"
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 import FormContentLoader from "../components/loaders/formContentLoader";
 
-const CustomerAddPage = ({ history, match }) => {
-    const { id = "new" } = match.params
-    const [error, setError] = useState({ lastname: "", firstname: "", email: "", company: "" })
-    const [customer, setCustomer] = useState({ lastname: "", firstname: "", email: "", company: "" })
+const CustomerAddPage = ({history, match}) => {
+    const {id = "new"} = match.params
+    const [error, setError] = useState({lastname: "", firstname: "", email: "", company: ""})
+    const [customer, setCustomer] = useState({lastname: "", firstname: "", email: "", company: ""})
     const [editing, setEditing] = useState(false)
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const fetchCustomer = async id => {
         try {
-            const { firstname, lastname, email, company } = await customerApi.find(id)
-            setCustomer({ firstname, lastname, email, company })
-            setLoading(false)
+            const {firstname, lastname, email, company} = await customerApi.find(id)
+            setCustomer({firstname, lastname, email, company})
         } catch (e) {
             console.log(e.response)
             toast.error('Impossible de chargé les client')
@@ -25,15 +24,15 @@ const CustomerAddPage = ({ history, match }) => {
     }
     useEffect(() => {
         if (id !== "new") {
-            setLoading(true)
             setEditing(true)
-            fetchCustomer(id)
+            setLoading(false)
+            fetchCustomer(id).then(resp => setLoading(false))
         }
     }, [id]);
 
-    const handleChange = ({ currentTarget }) => {
-        const { name, value } = currentTarget
-        setCustomer({ ...customer, [name]: value })
+    const handleChange = ({currentTarget}) => {
+        const {name, value} = currentTarget
+        setCustomer({...customer, [name]: value})
     }
 
     const handleSubmit = async e => {
@@ -44,16 +43,16 @@ const CustomerAddPage = ({ history, match }) => {
                 toast.success('Le client a bien ete modifié 😍')
             } else {
                 await customerApi.create(customer)
-                toast.error('Une erreur est survennue 😫')
+                toast.success('Le client a bien ete creer 😍')
                 history.replace("/customers")
             }
             setError({})
-        } catch ({ response }) {
-            const { violations } = response.data
+        } catch ({response}) {
+            const {violations} = response.data
             toast.error('Donner non valide 😪')
             if (violations) {
                 const apiErrors = {}
-                violations.forEach(({ propertyPath, message }) => apiErrors[propertyPath] = message)
+                violations.forEach(({propertyPath, message}) => apiErrors[propertyPath] = message)
                 setError(apiErrors)
             }
         }
@@ -62,19 +61,19 @@ const CustomerAddPage = ({ history, match }) => {
         <>
             <div className="d-flex justify-content-between align-items-center">
                 {!editing && <h1>Creation d'un client</h1> || <h1>Modification du client</h1>}
-                <Link to="/customers" className="btn btn-outline-dark bg-light"><FaBackward /></Link>
+                <Link to="/customers" className="btn btn-outline-dark bg-light"><FaBackward/></Link>
             </div>
             {!loading && <form onSubmit={handleSubmit}>
                 <Field name="firstname" label="Nom" placeholder="Nom de famille" value={customer.firstname}
-                    onChange={handleChange} error={error.firstname} />
+                       onChange={handleChange} error={error.firstname}/>
                 <Field name="lastname" label="Prenom" placeholder="Prenom" value={customer.lastname}
-                    onChange={handleChange} error={error.lastname} />
+                       onChange={handleChange} error={error.lastname}/>
                 <Field name="email" label="E-mail" placeholder="Adresse e-mail" type="email" value={customer.email}
-                    onChange={handleChange} error={error.email} />
+                       onChange={handleChange} error={error.email}/>
                 <Field name="company" label="Entreprise" placeholder="Entreprise" value={customer.company}
-                    onChange={handleChange} error={error.company} />
+                       onChange={handleChange} error={error.company}/>
                 <div className="form-group">
-                    <button type="submit" className="btn btn-outline-dark bg-light"><FaSave /></button>
+                    <button type="submit" className="btn btn-outline-dark bg-light"><FaSave/></button>
                 </div>
             </form>}
             {loading && <FormContentLoader/>}
--- assets/js/services/customerApi.js
+++ assets/js/services/customerApi.js
@@ -1,10 +1,53 @@
 import axios from 'axios'
+import cache from "./cache";
 
-const findAll = () => axios.get("http://localhost:8000/api/customers").then(resp => resp.data["hydra:member"])
-const deleteCustomer = id => axios.delete("http://localhost:8000/api/customers/" + id)
-const find = id => axios.get("http://localhost:8000/api/customers/" + id).then(response => response.data)
-const update = (id, customer) => axios.put("http://localhost:8000/api/customers/" + id, customer)
-const create = customer => axios.post("http://localhost:8000/api/customers", customer)
+const findAll = async () => {
+    const cachedCustomers = await cache.get('customers')
+    if (cachedCustomers) return cachedCustomers
+    return axios.get("http://localhost:8000/api/customers").then(resp => {
+        const customers = resp.data["hydra:member"]
+        cache.set("customers", customers)
+        return customers
+    })
+}
+
+const deleteCustomer = id => axios.delete("http://localhost:8000/api/customers/" + id).then(async response => {
+    const cachedCustomers = await cache.get('customers')
+    cachedCustomers && cache.set("customers", cachedCustomers.filter(c => c.id !== id))
+    return response
+})
+
+const find = async id => {
+    const cachedCustomers = await cache.get("customers." + id)
+    if (cachedCustomers) return cachedCustomers
+    return axios.get("http://localhost:8000/api/customers/" + id).then(response => {
+        const customer = response.data
+        cache.set('customers.' + id, customer)
+        return customer
+    })
+}
+
+const update = (id, customer) => axios.put("http://localhost:8000/api/customers/" + id, customer).then(async response => {
+    const cachedCustomers = await cache.get('customers')
+    const cachedCustomer = await cache.get('customers.' + id)
+    if (cachedCustomer) {
+        cache.set('customers.' + id, response.data)
+    }
+    if (cachedCustomers) {
+        const index = cachedCustomers.findIndex(c => c.id === +id)
+        cachedCustomers[index] = response.data
+        cachedCustomers && cache.set("customers", cachedCustomers)
+    }
+
+    return response
+})
+
+const create = customer => axios.post("http://localhost:8000/api/customers", customer).then(async response => {
+    const cachedCustomers = await cache.get('customers')
+    cachedCustomers && cache.set("customers", [...cachedCustomers, response.data])
+    return response
+})
+
 export default {
     findAll,
     find,
